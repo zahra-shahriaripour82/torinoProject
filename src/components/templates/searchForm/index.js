ca244aa3289a08cfc0580afecf20b242113a6dbe
Zahra shahriaripour <zahra.shahriaripour8285@gmail.com>
2025-03-10 23:08:03 +0330

    date picker added

--- src/components/templates/searchForm/index.js
+++ src/components/templates/searchForm/index.js
@@ -1,36 +1,66 @@
+"use client"
+import { citiesList } from "@/core/constants/constants";
+
+import { DatePicker } from 'zaman';
+// import DatePicker from "react-multi-date-picker";
+// import persian_fa from "react-date-object/locales/persian_fa";
+// import persian from "react-date-object/calendars/persian";
+// import InputIcon from "react-multi-date-picker/components/input_icon";
+// import "react-multi-date-picker/styles/colors/green.css";
+
 import Calender from "../../../../public/icons/Calender";
 import Global from "../../../../public/icons/Global";
 import Location from "../../../../public/icons/Location";
-
 function SearchForm() {
   return (
-    <div className="w-full  md:w-fit mt-6 mx-auto p-4 ">
-      <h2 className="text-center text-3xl font-medium text-[#595959]">
+    <div className="w-full md:w-fit  mx-auto p-4 mt-10 ">
+      <h2 className="text-center md:text-[28px] font-medium text-[#595959] mb-8">
         <span className="text-primary">تورینو</span> برگزار کننده بهترین تور های
         داخلی و خارجی
       </h2>
 
-      <form className="relative bg-white border p-4 mt-5 rounded-3xl  ">
+      <form className="relative bg-white border   p-4  rounded-3xl  ">
         <div className="grid  grid-cols-2 md:grid-cols-4 gap-4">
           <div className="relative">
             <div className="absolute right-3 top-1/2 -translate-y-1/2">
-              <Location className="w-5 h-5 text-gray-500" />
+              <Location className="w-5 h-5 text-gray-400" />
             </div>
-            <select className="w-full  border rounded-xl p-2 "></select>
+            <select className="w-full  border rounded-xl py-2 px-10 md:border-none focus:ring-2 focus:ring-primary outline-none text-right appearance-none ">
+            <option value="default">مبداء</option>
+              {citiesList.map((city) => (
+                <option key={city.id} value={city.id} >{city.name}</option>
+              ))}
+            </select>
           </div>
           <div className="relative">
             <div className="absolute right-3 top-1/2 -translate-y-1/2">
               <Global className="w-5 h-5 text-gray-400" />
             </div>
-            <select className="w-full py-2 px-10 border rounded-xl p-2 "></select>
-          </div>
-          <div className="relative col-span-2 md:col-span-1 border px-3 py-6 rounded-lg md:w-fit">
-            <div className="absolute right-3 top-1/2 -translate-y-1/2 mx-auto">
-              <Calender className="h-5 w-5 text-rose-800 bg-primary" />
-            </div>
+            <select className="w-full  border rounded-xl py-2 px-10 md:border-none focus:ring-2 focus:ring-primary outline-none text-right appearance-none ">
+            <option value="default">مقصد</option>
+              {citiesList.map((city) => (
+                <option key={city.id} value={city.id} >{city.name}</option>
+              ))}
+            </select>
           </div>
 
-          <button className=" w-full h-full col-span-2 md:col-span-1 bg-primary text-white px-4 py-2 rounded-2xl  hover:bg-primary/90 md:mr-2 ">
+          <div className="relative col-span-2 md:col-span-1 border px-3 py-2 rounded-lg  md:w-fit ">
+            <div className="absolute right-3 top-1/2 -translate-y-1/2 mx-auto">
+              <Calender className="h-5 w-5 text-gray-400" />
+            </div>
+            <DatePicker
+                  round="x2"
+                  accentColor="#28A745"
+                  // onChange={(e) => onChange({ 
+                  //   startDate: e.from ? DateToIso(e.from) : null, 
+                  //   endDate: e.to ? DateToIso(e.to) : null 
+                  // })}
+                  range
+                  className="w-full"
+                />
+          </div>
+
+          <button className=" w-full h-full col-span-2 md:col-span-1 bg-primary text-white px-4 py-2 rounded-xl   hover:bg-primary/90 md:mr-2 ">
             جستجو
           </button>
         </div>
--- src/core/constants/constants.js
+++ src/core/constants/constants.js
@@ -4,5 +4,14 @@
     { id: 3, href: "/about-us", title: "درباره ما" },
     { id: 4, href: "/contact-us", title: "تماس با ما" },
  ]
-
- export {navList}
+const citiesList=[
+ {id : 1 , name: "تهران"},
+    {id : 2 , name: "سنندج"},
+    {id : 3 , name: "مادرید"},
+    {id : 4 , name: "اصفهان"},
+    {id : 5 , name: "سلیمانیه تور"},
+    {id : 6 , name: "هولر"},
+    {id : 7 , name: "مازندران"},
+    {id : 8 , name: "افرود سنتر"},
+]
+ export {navList ,citiesList}
