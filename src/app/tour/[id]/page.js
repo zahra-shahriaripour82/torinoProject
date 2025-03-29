4ed7e4997fdcba25585847d37da5bc8d1ef6e962
Zahra shahriaripour <zahra.shahriaripour8285@gmail.com>
2025-03-29 20:47:06 +0330

    tour details page a little bite created

--- src/app/tour/[id]/page.js
+++ src/app/tour/[id]/page.js
@@ -1,12 +1,16 @@
 import TourDetailPage from '@/components/templates/TourdDetailsPage/TourDetailPage'
-import React from 'react'
+import { serverFetch } from '@/core/services/http'
 
-function TourDatail({params}) {
-    console.log(params);
+
+ async function TourList({ params }) {
+    const tourData = await serverFetch(`tour/${params.id}`, null, {
+        cache: "no-store",
+      })
+   
     
   return (
-   <TourDetailPage/>
+    <TourDetailPage tourData={tourData}/>
   )
 }
 
-export default TourDatail
+export default TourList
--- src/components/templates/TourdDetailsPage/TourDetailPage.js
+++ src/components/templates/TourdDetailsPage/TourDetailPage.js
@@ -2,12 +2,20 @@ import Image from "next/image"
 
 
 function TourDetailPage({tourData}) {
-  
+  // caculate duration day
+  const start = new Date(tourData.startDate);
+  const end = new Date(tourData.endDate);
+  const day = Math.ceil((end - start) / (1000 * 60 * 60 * 24))
+  const night=day-1;
+  console.log(tourData);
   
   return (
     <div>
-      dddd
-      {/* <Image src={toursData.image} alt={toursData.title}/> */}
+    <Image src={tourData.image} alt={tourData.title} width={330} height={220}/>
+     <div>
+      <h1>{tourData.title}</h1>
+<p>{day}روز و {night} شب</p>
+     </div>
     </div>
   )
 }
