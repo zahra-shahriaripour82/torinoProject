2f43f66dc4642c2063b9a7c63955395d9bf9c728
Zahra shahriaripour <zahra.shahriaripour8285@gmail.com>
2025-04-23 11:58:19 +0330

    peyment page is complited

--- src/components/templates/peyment/page.js
+++ src/components/templates/peyment/page.js
@@ -13,20 +13,20 @@ function Peyment({params:status}) {
     
 if (status === "==success"){
     return (
-        <div className=" flex items-center justify-center mt-12 rounded-lg">
+        <div className="  flex  items-center justify-center mt-8 rounded-lg">
 
+{/* w-full max-w-[1440px]  */}
+    <div className="bg-gray-50  rounded-lg px-4 py-4 md:flex items-center justify-center grid  ">
        
-    <div className="bg-gray-50  rounded-lg w-full max-w-[1440px] flex items-center justify-center">
-       
- <Image  src={"/images/peyment.jpg"} alt="پرداخت موفق" width={400} height={400}/>
-     <div>
-        <p className="text-sm md:text-lg font-normal text-primary ">{data?.data?.firstName || ""} عزیز پرداخت شما با موفقیت انجام شد.</p>
+ <Image  src={"/images/peyment.jpg"} alt="پرداخت موفق" width={300} height={300}/>
+     
+        <div className="grid gap-3 ">
+     
+        <p className="text-sm md:text-lg font-normal text-primary  ">{data?.data?.firstName || ""} عزیز پرداخت شما با موفقیت انجام شد.</p>
 
-     </div>
-        <div className="grid">
             <p>تور شما با موفقیت روز شد میتوانید جزئیات تور رو در پروفایل خود مشاهد ه کنید </p>
             <Link href={"/profile/my-tours"}>
-            <p>مشاهده تور</p>
+            <p className="bg-primary text-center rounded-md p-3 text-white">مشاهده تور</p>
             </Link>
         </div>
     </div>
